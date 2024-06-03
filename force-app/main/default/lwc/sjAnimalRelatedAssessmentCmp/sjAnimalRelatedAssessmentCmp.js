64f456a7dd49adf05a82dee68224bcfd0d382965
laurentiue-sj <laurentiu@solutionjunkies.co.uk>
2024-06-03 23:18:59 +0100

    Related Assessments Update

--- force-app/main/default/lwc/sjAnimalRelatedAssessmentCmp/sjAnimalRelatedAssessmentCmp.js
+++ force-app/main/default/lwc/sjAnimalRelatedAssessmentCmp/sjAnimalRelatedAssessmentCmp.js
@@ -3,11 +3,12 @@ import { NavigationMixin } from "lightning/navigation";
 import { graphql, gql } from "lightning/uiGraphQLApi";
 
 // eslint-disable-next-line @salesforce/lwc-graph-analyzer/no-unresolved-parent-class-reference
-export default class SjAnimalRelatedAssessmentCmp extends NavigationMixin(
-  LightningElement,
-) {
+export default class SjAnimalRelatedAssessmentCmp extends NavigationMixin(LightningElement) {
   @api recordId;
-
+  assessments;
+  animals;
+  totalCount;
+  errors;
   /*
     There is a currently Known Issue {@link https://issues.salesforce.com/issue/a028c00000xGGwE/graphql-query-fails-prefetch-with-an-unknown-field-warning} with GraphQL wire adapters where this will cause the component to fail to load offline.   
     There is a workaround that can be implemented in this Knowledge Article {@link https://help.salesforce.com/s/articleView?language=en_US&id=000396405&type=1}.
@@ -20,29 +21,29 @@ export default class SjAnimalRelatedAssessmentCmp extends NavigationMixin(
           query animalWithRelatedAssessments($recordId: ID) {
             uiapi {
               query {
-                animalshelters__Animal__c (where: { Id: { eq: $recordId } }) {
-                    edges {
-                        node {
+                Animal:animalshelters__Animal__c (where: { Id: { eq: $recordId } }) {
+                  edges {
+                    node {
+                      Id
+                      Name {
+                        value
+                      }
+                      Assessment:animalshelters__Assessments__r {
+                        edges {
+                          node {
                             Id
+                            animalshelters__Assessment_Type__c {
+                              value
+                            }
                             Name {
-                                value
+                              value
                             }
-                            animalshelters__Assessments__r {
-                                edges {
-                                    node {
-                                        Id
-                                        animalshelters__Assessment_Type__c {
-                                            value
-                                        }
-                                        Name {
-                                            value
-                                        }
-                                    }
-                                }
-                                totalCount
-                            }
+                          }
                         }
+                        totalCount
+                      }
                     }
+                  }
                 }
               }
             }
@@ -56,14 +57,16 @@ export default class SjAnimalRelatedAssessmentCmp extends NavigationMixin(
     variables: "$graphqlVariables",
     operationName: "animalWithRelatedAssessments",
   })
-  graphqlResult({ data /* errors */ }) {
-    this.assessments = null;
-    const animals = data?.uiapi?.query?.animalshelters__Animal__c?.edges;
-    if (animals && animals[0]) {
-      this.assessments = animals[0].node.animalshelters__Assessments__r.edges.map((e) => e.node);
+  graphqlResult({ data,errors }) {
+    if (data) {
+      this.animals = data?.uiapi.query.Animal.edges;
+      this.assessments = this.animals[0].node.Assessment.edges.map((e) => e.node);
+      this.totalCount = this.animals[0].node.Assessment.totalCount;
+      console.log("no of assessments" + JSON.stringify(this.totalCount))
+    } else if (errors) {
+      this.errors = errors;
     }
   }
-  assessments;
 
   get graphqlVariables() {
     return {
@@ -76,7 +79,7 @@ export default class SjAnimalRelatedAssessmentCmp extends NavigationMixin(
     this[NavigationMixin.Navigate]({
       type: "standard__quickAction",
       attributes: {
-        actionName: "animalshelters__Assessments__r.view",
+        actionName: "relatedAssessments.view",
       },
       state: {
         recordId: id,
